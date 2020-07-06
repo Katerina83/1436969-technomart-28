0b2eab8829d9ee615142bf6de2e3dc1299c6af73
Katerina83 <K.kivisheva@gmail.com>
2020-07-06 15:55:31 +0300

    Update letter.js

--- js/letter.js
+++ js/letter.js
@@ -1,77 +1,77 @@
 var buttonMap = document.querySelector(".button-map");
-var letterPopup = document.querySelector(".letter-form");
-var letterClose = letterPopup.querySelector(".modal-close");
-var letterForm = letterPopup.querySelector(".letter-form");
-var loginLogin = letterPopup.querySelector(".surname");
-var eMail = loginPopup.querySelector(".mail");
+var letterClose = document.querySelector(".modal-close");
+var letterForm = document.querySelector(".letter-form");
+var loginLogin = document.querySelector(".surname");
+var eMail = document.querySelector(".mail");
 
 var isStorageSupport = true;
 var storage = "";
 
 var mapLink = document.querySelector(".map");
-var mapPopup = document.querySelector(".modal-map");
+var mapPopup = document.querySelector(".map-show");
 var mapClose = mapPopup.querySelector(".modal-close");
 
-mapLink.addEventListener("click", function (evt) {
+mapLink.addEventListener("click", function(evt) {
     evt.preventDefault();
     mapPopup.classList.add("modal-show");
-  });
+});
 
-  mapClose.addEventListener("click", function (evt) {
+mapClose.addEventListener("click", function(evt) {
     evt.preventDefault();
-    mapPopup.classList.remove("modal-show");
-  });
+    mapPopup.classList.add("modal-show");
+});
 
-  window.addEventListener("keydown", function (evt) {
+window.addEventListener("keydown", function(evt) {
     if (evt.keyCode === 27) {
-      if (mapPopup.classList.contains("modal-show")) {
-        evt.preventDefault();
-        mapPopup.classList.remove("modal-show");
-      }
+        if (mapPopup.classList.contains("modal-show")) {
+            evt.preventDefault();
+            mapPopup.classList.remove("modal-show");
+        }
     }
-  });
+});
 
 try {
-  storage = localStorage.getItem("login");
+    storage = localStorage.getItem("login");
 } catch (err) {
-  isStorageSupport = false;
+    isStorageSupport = false;
 }
 
-buttonMap.addEventListener("click", function (evt) {
+buttonMap.addEventListener("click", function(evt) {
     evt.preventDefault();
-    letterPopup.classList.add("modal-show");
-    letterPopup.classList.remove("modal-error");
+    letterForm.classList.add("modal-show");
+    letterForm.classList.remove("modal-error");
 
     if (storage) {
         loginLogin.value = storage;
         eMail.focus();
-      } else {
+    } else {
         loginLogin.focus();
-      }
-    });
-   
-letterClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    letterPopup.classList.remove("modal-show");
-  });
-  letterForm.addEventListener("submit", function (evt) {
-    if (!loginLogin.value || !eMail.value) {
-    evt.preventDefault();
-    letterPopup.classList.remove("modal-error");
-    letterPopup.offsetWidth = loginPopup.offsetWidth;
-    letterPopup.classList.add("modal-error");
-
-    }else {
-        localStorage.setItem("login", loginLogin.value);
-      }
+    }
 });
 
-window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      if (letterPopup.classList.contains("modal-show")) {
+letterClose.addEventListener("click", function(evt) {
+    evt.preventDefault();
+    letterForm.classList.remove("modal-show");
+});
+
+letterForm.addEventListener("submit", function(evt) {
+    if (!loginLogin.value || !eMail.value) {
         evt.preventDefault();
-        letterPopup.classList.remove("modal-show");
-        letterPopup.classList.remove("modal-error");
-      }
+        letterForm.classList.remove("modal-error");
+        letterForm.offsetWidth = loginPopup.offsetWidth;
+        letterForm.classList.add("modal-error");
+
+    } else {
+        localStorage.setItem("login", loginLogin.value);
     }
-  });
+});
+
+window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+        if (letterForm.classList.contains("modal-show")) {
+            evt.preventDefault();
+            letterForm.classList.remove("modal-show");
+            letterForm.classList.remove("modal-error");
+        }
+    }
+});
